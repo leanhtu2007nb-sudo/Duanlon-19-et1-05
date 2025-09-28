fc7a8cb1088ba2c3c3b441ac592192594957897e
leanhtu2007nb-sudo <leanhtu2007nb@gmail.com>
2025-09-28 09:35:09 +0700

    Add files via upload

---
+++ thunghiemC.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+	long long n;
+	printf("Nhap so nguyen duong N: ");
+	scanf("%lld",&n);
+
+	if (n <= 0){
+		do{
+			printf ("Ban vua nhap so am, vui long nhap lai: ");
+			scanf("%lld",&n);
+		} while (n <=0);
+	}
+
+	int m = log10(n)+1;
+	long long a[m];
+
+	for (long long i = 0; i < m; i++){
+		a[i] = n % 10;
+		n = n / 10;
+	}
+
+
+	int tong = 0;
+	for (long long i = 0; i < m; i++){
+		tong += a[i];
+
+	}
+
+	printf("\nTong cac chu so la: %d", tong);
+	return 0;
+
+
+}
